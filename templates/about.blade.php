84b0162b2f76bbe246e533cffd58b735806075af
Rafa Ramirez <raffe90@gmail.com>
2017-04-11 03:30:47 +0200

    Adds images to about & gen improvements on styling

--- templates/about.blade.php
+++ templates/about.blade.php
@@ -20,6 +20,9 @@
       <div class="col-md-4 col-lg-3">
         <img class="iphone-preview" src="https://www.tryfra.me/wp-content/uploads/2017/04/iphone-first.png" alt="iPhone Preview (First Shot)">
       </div>
+      <img src="https://www.tryfra.me/wp-content/uploads/2017/04/zigzag-between-a-b.png" alt="This is a decorative zigzag" class="zigzag-decoration">
+      <img src="https://www.tryfra.me/wp-content/uploads/2017/04/big-circle-between-a-b.png" alt="This is a decorative circle" class="circle-decoration">
+      <img src="https://www.tryfra.me/wp-content/uploads/2017/04/trigangle-section-a.png" alt="This is a decorative triangle" class="triangle-decoration">
     </section>
     <section class="row section-c">
       <div class="col-md-4 col-lg-3 offset-lg-2 offset-md-1">
@@ -33,8 +36,11 @@
       <div class="col">
         <img class="iphone-preview mobile" src="https://www.tryfra.me/wp-content/uploads/2017/04/iphone-ghost.png" alt="iPhone Preview (Ghost Feature)">
       </div>
+      <img src="https://www.tryfra.me/wp-content/uploads/2017/04/rect-section-b.png" alt="This is a decorative square" class="square-decoration">
+      <img src="https://www.tryfra.me/wp-content/uploads/2017/04/down-caret-b.png" alt="This is a decorative caret" class="down-caret-decoration">
     </section>
     <section class="row section-d">
+      <img class="zigzag-decoration" src="https://www.tryfra.me/wp-content/uploads/2017/04/zigzag-between-b-c.png" alt="This is just a decorative zigzag">
       <header class="col-md-6 offset-md-1 col-lg-5 offset-lg-2">
         <img src="https://www.tryfra.me/wp-content/uploads/2017/04/ahu.png" alt="The Result!">
         <h1>The <strong>result?</strong></h1>
@@ -43,8 +49,13 @@
       <div class="col-lg-3 col-md-4">
         <img class="iphone-preview" src="https://www.tryfra.me/wp-content/uploads/2017/04/iphone-result.png" alt="iPhone Preview (Result)">
       </div>
+      <img src="https://www.tryfra.me/wp-content/uploads/2017/04/side-triangle-section-d.png" alt="This is just some decorative image, pay no attention" class="side-caret-decoration">
     </section>
     <section class="row section-e">
+      <img class="polygon-decoration" src="https://www.tryfra.me/wp-content/uploads/2017/04/polygon-section-d.png" alt="This is a decorative polygon">
+      <img src="https://www.tryfra.me/wp-content/uploads/2017/04/big-circle-bet-c-d.png" alt="This is a decorative circle" class="circle-decoration">
+      <img src="https://www.tryfra.me/wp-content/uploads/2017/04/down-triangle-top-sect-d.png" alt="This is a decorative triangle" class="triangle-decoration">
+      <img src="https://www.tryfra.me/wp-content/uploads/2017/04/rect-section-d.png" alt="This is a decorative square" class="square-decoration">
       <img class="top-ih" src="https://www.tryfra.me/wp-content/uploads/2017/04/top-ih-section.png" alt="Another Decorative Image Here Goes">
       <div class="w-100"></div>
       <header class="offset-md-2 col-md-8">
@@ -62,6 +73,7 @@
       <img class="bottom-ih" src="https://www.tryfra.me/wp-content/uploads/2017/04/bottom-ih-section.png" alt="Decorative Image Here Goes">
     </section>
     <section class="row section-f">
+      <img class="zigzag-decoration" src="https://www.tryfra.me/wp-content/uploads/2017/04/zigzag-section-e.png" alt="This is a decorative zigzag">
       <header class="offset-lg-2 col-lg-8 offset-md-1 col-md-10">
         <h1 class="main-title">
           Subscribe
