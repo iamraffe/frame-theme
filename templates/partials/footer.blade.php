f2b2f3fa0f1087eb58b75fdd49fe1dd027a41ba0
Rafa Ramirez <raffe90@gmail.com>
2017-04-11 02:34:32 +0200

    Adds improved styling on mobile

--- templates/partials/footer.blade.php
+++ templates/partials/footer.blade.php
@@ -15,6 +15,19 @@
 
 <footer class="content-info main-footer mobile">
   <div class="container-fluid">
+    <div class="row mobile-social-links">
+      <div class="col justify-content-center d-flex">
+        <a href="http://instagram.com">
+          <img src="https://www.tryfra.me/wp-content/uploads/2017/04/insta-mobile.png" alt="Instagram Social Link">
+        </a>
+        <a href="http://facebook.com">
+          <img src="https://www.tryfra.me/wp-content/uploads/2017/04/face-mobile.png" alt="Facebook Social Link">
+        </a>
+        <a href="http://twitter.com">
+          <img class="twitter" src="https://www.tryfra.me/wp-content/uploads/2017/04/twitter-mobile.png" alt="Twitter Social Link">
+        </a>
+      </div>
+    </div>
     <div class="row">
       <div class="col-md-6 app-store-download-wrapper">
         <img id="app-store-trigger" src="https://www.tryfra.me/wp-content/uploads/2017/04/appStore.png" alt="App Store Icon">
